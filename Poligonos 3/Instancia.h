a47322fd6305f3c673b7494cb65564f2cb357d9d
Jose <jose.venturini@edu.pucrs.br>
2020-10-25 12:41:50 -0300

    init

---
+++ Poligonos 3/Instancia.h
@@ -0,0 +1,28 @@
+//
+//  Instancia.hpp
+//  OpenGLTest
+//
+//  Created by Márcio Sarroglia Pinho on 22/09/20.
+//  Copyright © 2020 Márcio Sarroglia Pinho. All rights reserved.
+//
+
+
+#ifndef Instancia_hpp
+#define Instancia_hpp
+#include <iostream>
+using namespace std;
+
+#include "Poligono.h"
+
+class Instancia{
+    //Poligono *modelo;
+    
+public:
+    Ponto posicao, escala;
+    float rotacao;
+    int modelo;
+    void desenha();
+};
+
+
+#endif /* Instancia_hpp */
